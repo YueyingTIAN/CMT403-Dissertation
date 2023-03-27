54fcff8cc7203b9dc11b2a3e1564ecc6040769d7
Yueying Tian <117082776+YueyingTIAN@users.noreply.github.com>
2023-03-27 11:13:16 +0100

    Add files via upload

---
+++ interfaces/index.ts
@@ -0,0 +1,14 @@
+import { Emission, FoodsType, EmissionPayload, EmissionModelType } from "./emission";
+import { RecurringEmission, PeriodicityType, WeekDays } from "./recurringEmission";
+import NavStatelessComponent from "./navigation";
+
+export {
+  NavStatelessComponent,
+  Emission,
+  FoodsType,
+  EmissionPayload,
+  EmissionModelType,
+  RecurringEmission,
+  PeriodicityType,
+  WeekDays,
+};
