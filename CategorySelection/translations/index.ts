a07992091289a76a93d6e6ad72d06fb4a293d2d3
Yueying Tian <117082776+YueyingTIAN@users.noreply.github.com>
2023-03-27 11:20:47 +0100

    Delete CategorySelection directory

--- CategorySelection/translations/index.ts
+++
@@ -1,9 +0,0 @@
-import en from "./en.json";
-import zh from "./zh.json";
-
-interface TranslationKeys {
-  CATEGORY_SELECTION_SCREEN_TITLE: string;
-  CATEGORY_SELECTION_SCREEN_SELECT_CATEGORY: string;
-}
-
-export { en, zh, TranslationKeys };
