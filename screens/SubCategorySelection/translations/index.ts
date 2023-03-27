2ed726f7b6d6507481579c0fae0da172dbd4710b
Yueying Tian <117082776+YueyingTIAN@users.noreply.github.com>
2023-03-27 11:22:27 +0100

    Add files via upload

---
+++ screens/SubCategorySelection/translations/index.ts
@@ -0,0 +1,9 @@
+import en from "./en.json";
+import zh from "./zh.json";
+
+interface TranslationKeys {
+  SUB_CATEGORY_SELECTION_SCREEN_TITLE: string;
+  SUB_CATEGORY_SELECTION_SCREEN_SELECT_SUB_CATEGORY: string;
+}
+
+export { en, zh, TranslationKeys };
